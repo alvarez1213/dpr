bac7b9b9ec6e6e0d4ef9d37c47f2ba85118c8942
Dmitry Melnikov <d.melnikov.alex@gmail.com>
2024-09-03 14:16:34 +0300

    add postgres settings

--- backend/my_cloud/my_cloud/settings.py
+++ backend/my_cloud/my_cloud/settings.py
@@ -73,13 +73,23 @@ WSGI_APPLICATION = 'my_cloud.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'my_cloud',
+        'USER': 'my_cloud_user',
+        'PASSWORD': 'cloud123',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
