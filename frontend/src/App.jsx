591824dd56f61ff28457273ee6cd959ea210dedc
Dmitry Melnikov <d.melnikov.alex@gmail.com>
2024-09-15 00:19:28 +0300

    React App initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+// Components
+
+
+function App() {
+  return (
+    <div className="App">
+      <div className='content'>
+        
+      </div>      
+    </div>
+  );
+}
+
+export default App;
