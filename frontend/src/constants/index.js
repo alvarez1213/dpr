5d7140c2a46207a26dd2d8f8cee6a32d2876d2e1
Dmitry Melnikov <d.melnikov.alex@gmail.com>
2024-09-26 23:21:03 +0300

    Change URL API

--- frontend/src/constants/index.js
+++ frontend/src/constants/index.js
@@ -1,2 +1,2 @@
-export const API_URL_STORAGE = "http://127.0.0.1:8000/storage/";
-export const API_URL_USERS = "http://localhost:8000/users/";
+export const API_URL_STORAGE = "http://127.0.0.1:8000/api/storage/";
+export const API_URL_USERS = "http://localhost:8000/api/users/";
